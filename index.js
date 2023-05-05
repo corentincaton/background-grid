397ebaca3ae28faf8bf4cc48e8e8397bf5ab583b
Corentin Caton <corentincaton@gmail.com>
2023-05-05 16:28:47 +0200

    Use class

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// Initalise grid buttons after DOM is loaded
-document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("showGridButton")
-    .addEventListener("click", function () {
-      createGrid("#content", 8, 20, "#6495ed");
-    });
-  document
-    .getElementById("showResponsiveGridButton")
-    .addEventListener("click", function () {
-        createResponsiveGrid("#content", [768, 992, 1200], 2, 16, [3, 4, 8], [20, 24, 30], "#6495ed");
-    });
-  document
-    .getElementById("hideGridButton")
-    .addEventListener("click", function () {
-      removeGrid("#content");
-    });
-});
+const grid = new Grid("#content", 4, 20, "#567743");
+
+document
+  .getElementById("showGridButton")
+  .addEventListener("click", function () {
+    grid.showGrid();
+  });
+document
+  .getElementById("hideGridButton")
+  .addEventListener("click", function () {
+    grid.hideGrid();
+  });
